bb5dbacaf789c47847b8a578cdea3ace51fe78f5
Sigurdur Baldvin Fridriksson <sigurdurbaldvi@gmail.com>
2021-11-06 23:16:56 +0000

    Some changes

--- server.py
+++ server.py
@@ -2,8 +2,10 @@ import asyncio
 import websockets
 from test_program import Calculator
 
-async def echo(websocket, path):
+
+async def on_message(websocket, path):
     calc = Calculator()
+    await websocket.send("Enter an eqation: ")
     async for message in websocket:
         await websocket.send("Your message to me was:\r\n")
         await websocket.send(f">>> {message}\r\n")
@@ -16,7 +18,8 @@ async def echo(websocket, path):
         await websocket.send(f">>> {result}\r\n")
 
 async def main():
-    async with websockets.serve(echo, "localhost", 7560):
+
+    async with websockets.serve(on_message, "localhost", 7560):
         await asyncio.Future() # Run forever
 
 if __name__ == "__main__":
