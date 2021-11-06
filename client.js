304068c0a78241efd0f209d036bc5fcfeec3e6f5
Sigurdur Baldvin Fridriksson <sigurdurbaldvi@gmail.com>
2021-11-06 23:16:37 +0000

    Writing in the termnal works now

--- client.js
+++ client.js
@@ -1,3 +1,7 @@
+var terminal = new Terminal({
+  cursorBlink: "block",
+});
+
 async function connect() {
   return new Promise(function (resolve, reject) {
     var server = new WebSocket("ws://localhost:7560");
@@ -22,10 +26,40 @@ async function sendGreeting() {
     console.log("Obbobobb", error);
   }
 }
+
 document.getElementById("messageButton").addEventListener("click", (e) => {
   sendGreeting();
 });
 
-var terminal = new Terminal();
 terminal.open(document.getElementById("terminal"));
+let curr_line = "";
+let entries = [];
+
+terminal.onKey(function (key, ev) {
+  // Enter
+  console.log(key);
+  console.log(ev);
+  if (key.key == "\r") {
+    if (curr_line) {
+      entries.push(curr_line);
+      terminal.write("\r\n");
+      terminal.prompt();
+    }
+  } else {
+    console.log(curr_line);
+    curr_line += key.key;
+    terminal.write(key.key);
+  }
+});
+
+terminal.prompt = () => {
+  if (curr_line) {
+    let data = { method: "command", command: curr_line };
+    server.send(JSON.stringify(data));
+  }
+};
 terminal.prompt();
+
+window.onload = function () {
+  connect();
+};
