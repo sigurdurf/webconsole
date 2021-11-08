fdd3e1018b3e47129b6b9fd00749c31c49d9514d
Sigurdur Baldvin Fridriksson <sigurdurbaldvi@gmail.com>
2021-11-08 22:14:13 +0000

    Dynamic heroku ports

--- web/app.py
+++ web/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask.templating import render_template
 
@@ -8,4 +9,5 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
